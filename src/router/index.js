e7d0fd4c55895a920b994df1929f145caeb90972
Chhaya <133883012+chha0031@users.noreply.github.com>
2024-12-06 02:54:58 +0000

    adds: frontend for reviews

---
+++ src/router/index.js
@@ -0,0 +1,17 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../components/Home.vue";
+import ReviewsList from "../components/ReviewsList.vue";
+import ReviewDetails from "../components/ReviewDetails.vue";
+
+const routes = [
+  { path: "/", component: Home },
+  { path: "/reviews", component: ReviewsList },
+  { path: "/review/:id", component: ReviewDetails },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
